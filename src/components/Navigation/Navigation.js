7f07eb5eb0e137eed136f5b59bfc0aa2e377dd84
Carlos Gomez <CGomez374@yahoo.com>
2022-03-14 21:09:01 -0700

    updated packages

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NavStyle.css';
-function Navigation(){
-    return(
-        <nav className="nav">
-            <h1>Movies</h1>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/search'>search</Link>
-                </li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">TV Shows</a></li>
-            </ul>
-        </nav>
-    )
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavStyle.css";
+function Navigation() {
+  return (
+    <nav className="nav">
+      <h1>Movies</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">search</Link>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">TV Shows</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 import Styles from "./Showcase.module.css";
-import Navigation from '../Navigation/Navigation';
-
+// import Navigation from '../Navigation/Navigation';
 
 const Showcase = () => {
-    return (
-        <header className={Styles.showcase}>
-            <h1>welcome to the movies</h1>
-        </header>
-    )
-}
+  return (
+    <header className={Styles.showcase}>
+      <h1>welcome to the movies</h1>
+    </header>
+  );
+};
 
-export default Showcase
+export default Showcase;
